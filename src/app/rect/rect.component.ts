809481e2f75eb57fe792b1279975cb5d835a1623
Armen Stepanyan <st.armen91@gmail.com>
2018-12-28 11:03:15 +0300

    Load Files

---
+++ src/app/rect/rect.component.ts
@@ -0,0 +1,114 @@
+import {Component, OnInit, HostListener, ViewChild, ElementRef} from '@angular/core';
+
+@Component({
+    selector: 'app-rect',
+    templateUrl: './rect.component.html',
+    styleUrls: ['./rect.component.css']
+})
+export class RectComponent implements OnInit {
+
+    isActive: Boolean = false;
+    isMouseClicked: Boolean = false;
+    isRotateBtnClicked: Boolean = false;
+
+    corner: string;
+
+    @ViewChild('shadow') shadow: ElementRef;
+
+
+    rectStyles = {width: 300, height: 170, left: 200, top: 100, transform: 'rotate(0deg)'};
+    shadowStyles = {...this.rectStyles};
+
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+    onRotateBtnMouseDown() {
+        this.isRotateBtnClicked = true;
+    }
+
+    @HostListener('document:mousemove', ['$event'])
+    onMouseMove(e) {
+
+        if (!this.isActive) {
+            return;
+        }
+
+        if (this.isMouseClicked) {
+
+            switch (this.corner) {
+                case 'top-left':
+                    this.shadowStyles = {
+                        ...this.shadowStyles,
+                        left: e.pageX,
+                        top: e.pageY,
+                        width: this.getValue(this.shadowStyles.width - (e.pageX - this.shadowStyles.left)),
+                        height: this.getValue(this.shadowStyles.height - (e.pageY - this.shadowStyles.top))
+                    };
+                    break;
+                case 'top-right':
+                    this.shadowStyles = {
+                        ...this.shadowStyles,
+                        width: this.getValue(e.pageX - this.shadowStyles.left),
+                        top: e.pageY,
+                        height: this.getValue(this.shadowStyles.height - (e.pageY - this.shadowStyles.top))
+                    };
+                    break;
+                case 'bottom-right':
+                    this.shadowStyles = {
+                        ...this.shadowStyles,
+                        width: this.getValue(e.pageX - this.shadowStyles.left),
+                        height: this.getValue(e.pageY - this.shadowStyles.top)
+                    };
+                    break;
+                case 'bottom-left':
+                    this.shadowStyles = {
+                        ...this.shadowStyles,
+                        left: e.pageX,
+                        height: this.getValue(e.pageY - this.shadowStyles.top),
+                        width: this.getValue(this.shadowStyles.width - (e.pageX - this.shadowStyles.left)),
+                    };
+
+                    break;
+            }
+
+        } else if (this.isRotateBtnClicked) {
+            const centerX = this.shadowStyles.left + (this.shadowStyles.width / 2);
+            const centerY = (this.shadowStyles.top) + (this.shadowStyles.height / 2);
+            const mouseX = e.pageX;
+            const mouseY = e.pageY;
+            const radians = Math.atan2(mouseX - centerX, mouseY - centerY);
+            const degree = (radians * (180 / Math.PI) * -1) + 180;
+            this.shadowStyles = {...this.shadowStyles, transform: `rotate(${degree}deg)`};
+
+        }
+
+    }
+
+    getValue = (val) => {
+        return val < 0 ? 0 : val;
+    }
+
+    @HostListener('document:mouseup', ['$event'])
+    onMouseUp(e) {
+        if (this.isActive && (this.isMouseClicked || this.isRotateBtnClicked)) {
+            console.log('unclick');
+            this.rectStyles = {...this.shadowStyles};
+        }
+        this.isMouseClicked = false;
+        this.isRotateBtnClicked = false;
+
+    }
+
+
+    onMouseDown($event, corner) {
+        this.isMouseClicked = true;
+        this.corner = corner;
+        // console.log(this.isMouseClicked, corner);
+    }
+
+
+}
